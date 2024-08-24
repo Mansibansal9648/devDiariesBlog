a3e3204a5f78b8d01c9649302119d93062fbf7bd
mansibansal9648 <mansibansal12345@gmail.com>
2024-08-24 23:58:18 +0530

    timestamp coversion in ist using moment

--- src/components/myBlog/myBlog.jsx
+++ src/components/myBlog/myBlog.jsx
@@ -6,6 +6,8 @@ import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import { Link } from "react-router-dom";
 import pimg from "../../assets/images/catwallpaper.jpg";
+import moment from "moment";
+import 'moment-timezone';
 
 function MyBlog() {
   const {
@@ -77,8 +79,11 @@ function MyBlog() {
                         <div className="post-detail">
                           <h6 className="fw-bold">{item.title}</h6>
                           <p>{}</p>
-                          <span>
+                          {/* <span>
                             {item.createdAt == item.updatedAt?<span>Created : {item.createdAt.split("GMT")[0]}</span>:<span>Updated : {item.updatedAt.split("GMT")[0]}</span>}
+                          </span> */}
+                          <span>
+                            {item.createdAt==item.updatedAt?<span>Created At :{ moment.parseZone(item?.createdAt)?.tz('Asia/Kolkata')?.format('ddd MMMM D, YYYY [at] h:mm:ss A')}</span>:<span>Updated At : {moment.parseZone(item?.updatedAt)?.tz('Asia/Kolkata')?.format('ddd MMMM D, YYYY [at] h:mm:ss A')}</span>}
                           </span>
                         </div>
                         <div className="mid d-flex gap-2 align-self-end">
