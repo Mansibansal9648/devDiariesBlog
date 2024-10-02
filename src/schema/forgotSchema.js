482b0eba35c0976ed9823070aa95f32405b1ebd9
devMrSY <frankSooraj@gmail.com>
2024-10-02 12:52:07 +0530

    delete shema

--- src/schema/forgotSchema.js
+++
@@ -1,7 +0,0 @@
-import * as Yup from 'yup';
-
-export const forgotSchema = Yup.object({
-  email: Yup.string()
-    .matches(/^[^\s@]+@[^\s@]+\.[^\s@]+$/, 'Please enter a valid email address')
-    .required('Please enter your email'),
-});
